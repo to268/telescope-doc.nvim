2034623d06860ce3672f714a8966b16c9ae4f7ef
Guillot Tony <tony.guillot@protonmail.com>
2024-10-06 17:49:30 +0200

    Updated copyright year

--- lua/telescope-doc/open-file.lua
+++ lua/telescope-doc/open-file.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright © 2021 Guillot Tony <tony.guillot@protonmail.com>
+Copyright © 2024 Guillot Tony <tony.guillot@protonmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
